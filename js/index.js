df9bcfc8d45dd4589cf8a5bb799d977b11f701a4
AlexisBazila <alexisbazila@proton.me>
2024-02-09 16:02:57 -0300

    Estilos

--- js/index.js
+++ js/index.js
@@ -6,9 +6,16 @@ let Equipments = new Array();
 if (localStorage.getItem("Equipments")) {
   Equipments = JSON.parse(localStorage.getItem("Equipments"));
 }
+let Rentals = new Array();
+if (localStorage.getItem("Rentals")){
+  Rentals = JSON.parse(localStorage.getItem("Rentals"));
+}
 
 let countCus = document.getElementById("countCus");
 countCus.innerHTML=`${Customers.length}`
 
 let countEqui = document.getElementById("countEqui");
 countEqui.innerHTML=`${Equipments.length}`
+
+let countRent = document.getElementById("countRent");
+countRent.innerHTML=`${Rentals.length}`
